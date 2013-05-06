ddd02a84b940e8af0b7acbf809aa3e01d8e57334
zhangxu <zhangxu@admaster.com.cn>
2013-05-06 11:41:30 +0800

    again commit

---
+++ test/src/test/java/admaster/zhangxu/ObectStoreTest.java
@@ -0,0 +1,11 @@
+package admaster.zhangxu;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhangxu
+ * Date: 13-4-25
+ * Time: 下午3:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class ObectStoreTest {
+}
